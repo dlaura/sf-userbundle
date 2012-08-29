25c08b61ed6b3c2e83a099ed06ff1d77c3ad8200
Diana Laura Garcia <diana.laura.garcia@gmail.com>
2012-08-29 02:52:47 -0400

    created utilities classes

---
+++ Util/CodeGenerator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Onfan\UserBundle\Util;
+
+class CodeGenerator
+{
+    /**
+     * Generate Verification Code
+     *
+     * @return string 
+     */
+    static function generateVerificationCode()
+    {
+            $verificationCode = hash('sha256', uniqid(null, true));
+            $verificationCode = substr($verificationCode, 0, 7);
+            return $verificationCode;
+    }
+    
+    /**
+     * Generate Session Access Token
+     * 
+     * @return string
+     */
+    static function generateSessionAccessToken()
+    {
+        return hash('sha256', uniqid(null, true));
+    }
+}
