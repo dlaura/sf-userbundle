55e27bb953a4cefc29bf85cd558f970bbd56e84e
Diana Laura Garcia <diana.laura.garcia@gmail.com>
2012-09-03 15:54:11 -0400

    fixed AccessToken controller with Symfony's authentication and GET method

--- Controller/AccessTokenController.php
+++ Controller/AccessTokenController.php
@@ -20,9 +20,9 @@ class AccessTokenController extends Controller
 {
 
     /**
-     * Create a new user
+     * Create a new access token for an authenticated user
      * @Route("/accesstoken", defaults={"_format"="json"})
-     * @Method({"POST"})
+     * @Method({"GET"})
      */
     public function getAccessTokenAction()
     {
@@ -32,102 +32,25 @@ class AccessTokenController extends Controller
         $request = Request::createFromGlobals();
         $response = new Response();
         
-        if ($request->request->has('type')) {
-            $type = $request->request->get('type');
-            switch ($type) {
-                case 'userpass':
-                    if ($request->request->has('username') && $request->request->has('password')) {
-                        $username = $request->request->get('username');            
-                        $password = $request->request->get('password');
-                        
-                        // check user's credentials
-                        $user = $em->getRepository('OnfanUserBundle:User\User')->findOneBy(array(
-                            'username' => $username,
-                            'password' => $password
-                        ));
-                        
-                        if ($user) {
-                            // create new token
-                            $token = new AccessToken();
-                            $token->setUser($user);
-                            
-                            // persist token
-                            $em->persist($token);
-                            $em->flush();
-                            
-                            // serialize token
-                            $output = $serializer->serialize($token, 'json');
-                            
-                            $response->setContent($output);
-                            return $response;                            
-                        } else {
-                            $response->setStatusCode(HttpCodes::HTTP_FORBIDDEN, "Invalid user/password credentials");
-                            return $response;
-                        }
-                    } else {
-                        $response->setStatusCode(HttpCodes::HTTP_BAD_REQUEST, "You must pass username and password");
-                        return $response;
-                    }
+        
+        $user = $this->getUser();
+        if ($user) {
+            // create new access token
+            $token = new AccessToken();
+            $token->setUser($user);
 
-                    break;
-                case 'facebook':
-                    if ($request->request->has('fb_access_token')) {
-                        $fb_access_token = $request->request->get('fb_access_token');                        
-                        
-                        // validate facebook access token
-                        // with PHP FB API
-                        
-                        if (true) {
-                            // if there is already a user with this FBUser.email
-                            if (true) {
-                                // update existing user with FB.id
-                                // load user into $user
-                            } else {
-                                // create a new user
-                                // set FB.id, and other data from FB
-                                // load user into $user
-                            }
-                            
-                            // create new token
-                            $token = new AccessToken();
-                            $token->setUser($user);
-                            
-                            // persist token
-                            $em->persist($token);
-                            $em->flush();
-                            
-                            // serialize token
-                            $output = $serializer->serialize($token, 'json');
-                            
-                            $response->setContent($output);
-                            return $response;                            
-                        } else {
-                            $response->setStatusCode(HttpCodes::HTTP_FORBIDDEN, "Invalid Facebook access token");
-                            return $response;
-                        }
-                    } else {
-                        $response->setStatusCode(HttpCodes::HTTP_BAD_REQUEST, "You must pass a valid Facebook access token");
-                        return $response;
-                    }
+            // persist token
+            $em->persist($token);
+            $em->flush();
 
+            // serialize token
+            $output = $serializer->serialize($token, 'json');
 
-                    break;
-
-                default:
-                    // undefined authentication type
-                    $response->setStatusCode(HttpCodes::HTTP_NOT_ACCEPTABLE, "Authentication method not acceptable");
-                    return $response;
-                    break;
-            }
+            $response->setContent($output);
+            return $response;                            
         } else {
-            // authentication type required
-            $response->setStatusCode(HttpCodes::HTTP_NOT_FOUND, "Authentication method not found");
+            $response->setStatusCode(HttpCodes::HTTP_FORBIDDEN, "Invalid user/password credentials");
             return $response;
-            break;
         }
-   
     }
-    
-
-    
 }
