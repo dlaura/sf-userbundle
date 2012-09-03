62845252bbc53a835fe60162f161c3761b565b0c
Diana Laura Garcia <diana.laura.garcia@gmail.com>
2012-09-03 15:55:16 -0400

    added Security Listener Factory WsseUserPasswordFactory in bundle

--- OnfanUserBundle.php
+++ OnfanUserBundle.php
@@ -3,7 +3,17 @@
 namespace Onfan\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Onfan\WSSEUserPasswordBundle\Security\Factory\Factory as WsseUserPasswordFactory;
+
 
 class OnfanUserBundle extends Bundle
 {
+    public function build(ContainerBuilder $container) {
+        parent::build($container);
+        
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new WsseUserPasswordFactory());
+    }
 }
