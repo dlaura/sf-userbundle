e7b591b8cbecb12422a793a9e60b15006d11f689
Diana Laura Garcia <diana.laura.garcia@gmail.com>
2012-09-04 10:30:36 -0400

    added Security Listener Factory: WsseFacebookTokenFactory

--- OnfanUserBundle.php
+++ OnfanUserBundle.php
@@ -7,6 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 use Onfan\WSSEUserPasswordBundle\Security\Factory\Factory as WsseUserPasswordFactory;
 use Onfan\WSSEAccessTokenBundle\Security\Factory\Factory as WsseAccessTokenFactory;
+use Onfan\WSSEFacebookTokenBundle\Security\Factory\Factory as WsseFacebookTokenFactory;
 
 
 class OnfanUserBundle extends Bundle
@@ -17,5 +18,6 @@ class OnfanUserBundle extends Bundle
         $extension = $container->getExtension('security');
         $extension->addSecurityListenerFactory(new WsseUserPasswordFactory());
         $extension->addSecurityListenerFactory(new WsseAccessTokenFactory());
+        $extension->addSecurityListenerFactory(new WsseFacebookTokenFactory());
     }
 }
